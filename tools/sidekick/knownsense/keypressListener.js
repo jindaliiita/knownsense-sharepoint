3ca4def5b63a3b3e20ae6f9dc4138f2f63cfea0b
Sagar Miglani <saga@adobe.com>
2023-12-05 12:00:16 +0530

    keypress event

--- tools/sidekick/knownsense/keypressListener.js
+++ tools/sidekick/knownsense/keypressListener.js
@@ -1,5 +1,6 @@
 document.addEventListener('keydown', function(event) {
     // Check if the pressed key is an alphabet
+    event.stopPropagation();
     if (/^[a-zA-Z0-9]$/.test(event.key)) {
       console.log('pressed key:', event.key);
       // Add your custom logic here
