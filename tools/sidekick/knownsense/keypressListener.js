22cdb1b16412b96dd51cfad93821b8b5e0f7f524
Sagar Miglani <saga@adobe.com>
2023-12-05 12:01:29 +0530

    keypress event

--- tools/sidekick/knownsense/keypressListener.js
+++ tools/sidekick/knownsense/keypressListener.js
@@ -1,8 +1,10 @@
-document.addEventListener('keydown', function(event) {
-    // Check if the pressed key is an alphabet
-    event.stopPropagation();
-    if (/^[a-zA-Z0-9]$/.test(event.key)) {
-      console.log('pressed key:', event.key);
-      // Add your custom logic here
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('keydown', function(event) {
+        // Check if the pressed key is an alphabet
+        event.stopPropagation();
+        if (/^[a-zA-Z0-9]$/.test(event.key)) {
+          console.log('pressed key:', event.key);
+          // Add your custom logic here
+        }
+      });
   });
