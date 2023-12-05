f0153d6aa93e8671c33e3d07aabc435a5be8dea1
piyushjindal <piyushjindal@adobe.com>
2023-12-05 13:29:32 +0530

    Franklin Sidekick 5

--- tools/sidekick/knownsense/knownsense.js
+++ tools/sidekick/knownsense/knownsense.js
@@ -1,29 +1,23 @@
 const getAllBlocksList = async () => {
   const url = "https://main--knownsense--jindaliiita.hlx.live/tools/sidekick/library.json";
   const result = await fetch(url);
+  let blockList = [];
 
   if (result.ok) {
     const textResult = await result.text();
-    console.log(textResult);
+    const jsonResult = JSON.parse(textResult);
+    blockList = jsonResult.data;
   } else {
     console.error('Error:', result.status);
   }
-  // fetch(url)
-  //   .then(response => response.text())
-  //   .then(html => {
-  //     // Create a temporary element to parse the HTML content
-  //     const tempElement = document.createElement('div');
-  //     tempElement.innerHTML = html;
 
-  //     // Access the block list from the rendered document
-  //     const blockList = tempElement.querySelectorAll('.block-list'); // Update this selector with the actual selector for your blocks
-
-  //     // Now you can do something with the block list, such as logging it to the console
-  //     console.log('Block List:', blockList);
-  //   })
-  //   .catch(error => {
-  //     console.error('Error fetching the document:', error);
-  //   });
+  return blockList;
 }
 
-await getAllBlocksList();
+const blocklist = await getAllBlocksList();
+
+const filterBlocklist = (text) => {
+  
+}
+
+console.log(blocklist);
