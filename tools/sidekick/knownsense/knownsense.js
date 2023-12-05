dda18bb39873909ec635630ec05d20ea86db51b8
piyushjindal <piyushjindal@adobe.com>
2023-12-05 10:52:38 +0530

    Franklin Sidekick 3

--- tools/sidekick/knownsense/knownsense.js
+++ tools/sidekick/knownsense/knownsense.js
@@ -1,17 +1,26 @@
-fetch('https://main--sunstar--hlxsites.hlx.page/tools/sidekick/library.html?plugin=blocks')
-  .then(response => response.text())
-  .then(html => {
-    // Create a temporary element to parse the HTML content
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = html;
+const getAllBlocksList = async () => {
+  const url = "https://main--knownsense--jindaliiita.hlx.live/tools/sidekick/library.json";
+  const result = await fetch(url);
 
-    // Access the block list from the rendered document
-    const blockList = tempElement.querySelectorAll('.block-list'); // Update this selector with the actual selector for your blocks
+  if (result) {
+    console.log(result.text());
+  }
+  // fetch(url)
+  //   .then(response => response.text())
+  //   .then(html => {
+  //     // Create a temporary element to parse the HTML content
+  //     const tempElement = document.createElement('div');
+  //     tempElement.innerHTML = html;
 
-    // Now you can do something with the block list, such as logging it to the console
-    console.log('Block List:', blockList);
-  })
-  .catch(error => {
-    console.error('Error fetching the document:', error);
-  });
-  
+  //     // Access the block list from the rendered document
+  //     const blockList = tempElement.querySelectorAll('.block-list'); // Update this selector with the actual selector for your blocks
+
+  //     // Now you can do something with the block list, such as logging it to the console
+  //     console.log('Block List:', blockList);
+  //   })
+  //   .catch(error => {
+  //     console.error('Error fetching the document:', error);
+  //   });
+}
+
+await getAllBlocksList();
