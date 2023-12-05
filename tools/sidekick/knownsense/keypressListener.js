18490890a042617c7ea56786643ae38ca66edc89
Sagar Miglani <saga@adobe.com>
2023-12-05 11:23:15 +0530

    keypress event

---
+++ tools/sidekick/knownsense/keypressListener.js
@@ -0,0 +1,7 @@
+document.addEventListener('keypress', (event) => {
+    var name = event.key;
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+}, false);
+console.log("keypress listener added");
