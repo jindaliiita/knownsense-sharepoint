5c2c7336f3ab9f323579cc0dd27b277b123f6529
Sagar Miglani <saga@adobe.com>
2023-12-05 11:28:44 +0530

    keypress event

--- tools/sidekick/knownsense/keypressListener.js
+++ tools/sidekick/knownsense/keypressListener.js
@@ -1,3 +1,7 @@
-$(document).keydown((event) => {
-    alert("key pressed");
-});
+var iframe = document.getElementsByTagName("iframe")[0];
+if (iframe) {
+    iframe.contentDocument.addEventListener("keypress", function(evt) {
+        console.log("iframe keypress: " + evt.which);
+        alert("button pressed");
+    }, false);
+}
